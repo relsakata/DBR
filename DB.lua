7e3ccc4e908176f880be2c8dc8da20fca5213943
Sakata <abstract.kenz@gmail.com>
2024-03-18 17:33:00 +0700

    Update DB.lua

--- DB.lua
+++ DB.lua
@@ -1,4 +1,4 @@
-repeat task.wait() until game:IsLoaded() task.wait(3)
+repeat task.wait() until game:IsLoaded() task.wait(6)
 local LocalPlayer = game.Players.LocalPlayer
 local Character = LocalPlayer.Character
 
@@ -24,5 +24,46 @@ for i,v in workspace.Map:GetChildren() do
         task.wait(.5)
     end
 end
+
 task.wait(1)
-loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")():Teleport(game.PlaceId)
+
+local JobIds = {}
+local nextPageCursor
+if not isfile("dbr-shop.json") then
+	writefile("dbr-shop.json", game:GetService("HttpService"):JSONEncode(JobIds))
+end
+
+local JobIds = game:GetService("HttpService"):JSONDecode(readfile("dbr-shop.json"))
+
+local function ServerHop()
+	local Site;
+	if not nextPageCursor then
+		Site = game:GetService("HttpService"):JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
+	else
+		Site = game:GetService("HttpService"):JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. nextPageCursor))
+	end
+	local ID = ""
+	if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+		nextPageCursor = Site.nextPageCursor
+	end
+	local num = 0;
+	for i,v in pairs(Site.data) do
+		ID = v.id
+		if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            if JobIds[ID] then
+                if tick() - JobIds[ID] >= 600 then
+                    JobIds[ID] = nil
+                elseif i ~= #Site.data then
+                    continue
+                else
+                    xpcall(ServerHop, warn)
+                end
+            end
+			JobIds[ID] = tick()
+			writefile("dbr-shop.json", game:GetService("HttpService"):JSONEncode(JobIds))
+			repeat game:GetService("TeleportService"):TeleportToPlaceInstance(game.placeId, ID) task.wait() until nil
+		end
+	end
+end
+
+xpcall(ServerHop, warn)
