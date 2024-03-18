e5db82bce2af61309fd1793c5a15c411858b6fbb
Sakata <abstract.kenz@gmail.com>
2024-03-18 14:21:24 +0700

    Update DB.lua

--- DB.lua
+++ DB.lua
@@ -1,15 +1,15 @@
-repeat task.wait() until game:IsLoaded() task.wait(2)
+repeat task.wait() until game:IsLoaded() task.wait(3)
 local LocalPlayer = game.Players.LocalPlayer
 local Character = LocalPlayer.Character
 
 --Wish
 if require(game:GetService("ReplicatedStorage")["_replicationFolder"].DragonBallUtils).PlayerHasAllDragonBalls(LocalPlayer) then
     Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Workspace").Map.Pedestal.Center.Position)
-    task.wait(.2)
+    task.wait(.5)
     fireproximityprompt(game:GetService("Workspace").Map.Pedestal.Center.Attachment.ProximityPrompt)
-    task.wait(.2)
+    task.wait(.5)
     firesignal(LocalPlayer.PlayerGui.Wish.Wishes.List.ZenkaiBoost.Button.MouseButton1Click)
-    task.wait(.2)
+    task.wait(.5)
     firesignal(LocalPlayer.PlayerGui.Wish.Accept.MouseButton1Click)
 end
 
@@ -21,7 +21,7 @@ for i,v in workspace.Map:GetChildren() do
         Character.HumanoidRootPart.CFrame = CFrame.new(v.Main.Position)
         task.wait(.5)
         fireproximityprompt(v.Main.Attachment.ProximityPrompt)
-        task.wait(.2)
+        task.wait(.5)
     end
 end
 task.wait(1)
